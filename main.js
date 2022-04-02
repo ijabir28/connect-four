0f8f6995c400da6daf624b51a48821095b3627e2
ijabir28 <jabir.kamal@northsouth.edu>
2022-04-02 14:47:06 +0600

    Take move from player.

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
+const mock_moves = [1, 2, 3, 4, 5, 6];
+
+
 function new_board() {
     return Array.from({length: 6}, () => Array(7).fill(0));
+}
+
+function take_move_from_player() {
+    return mock_moves.pop();
 }
 
 function start_game() {
     console.log('Game start....');
     const board = new_board();
     console.log(board);
+
+    while (true) {
+        const move = take_move_from_player();
+        console.log({move});
+    }
 }
 
 start_game()
