26847474e9db32808c0e251ecfe1851cc7e0c76d
ijabir28 <jabir.kamal@northsouth.edu>
2022-04-02 14:24:00 +0600

    Initialize new board.

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+function new_board() {
+    return Array.from({length: 6}, () => Array(7).fill(0));
+}
+
 function start_game() {
     console.log('Game start....');
+    const board = new_board();
+    console.log(board);
 }
 
 start_game()
