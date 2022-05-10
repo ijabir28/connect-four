7fca8a5235cd71edfc343567109dceedebc83590
ijabir28 <jabir.kamal@northsouth.edu>
2022-05-10 19:22:08 +0600

    refactor.

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const {range} = require("./util");
+const { range } = require("./util");
 
 const game_status = {
     player_zero_winner: 0,
@@ -35,12 +35,14 @@ function is_winner(board, player_no) {
 
     const offsets = [0, 1, 2, 3];
 
-    return row_indices.some(row_index =>
-        col_indices.some(col_index =>
-            direction.some(([row_direction, col_direction]) =>
-                offsets.every(offset => (
+    return (
+        row_indices.some(row_index =>
+            col_indices.some(col_index =>
+                direction.some(([row_direction, col_direction]) =>
+                    offsets.every(offset => (
                         is_valid_range(row_index + offset * row_direction, col_index + offset * col_direction, board) &&
                         board[row_index + offset * row_direction][col_index + offset * col_direction] === player_no
+                    )
                     )
                 )
             )
@@ -60,11 +62,11 @@ function is_game_over(board) {
 }
 
 function game_result(board) {
-    if(is_winner(board, 0)){ 
+    if (is_winner(board, 0)) {
         return game_status.player_zero_winner;
-    } else if(is_winner(board, 1)) {
+    } else if (is_winner(board, 1)) {
         return game_status.player_one_winner;
-    } else if(fill_up(board)) {
+    } else if (fill_up(board)) {
         return game_status.draw;
     } else {
         return game_status.playing;
@@ -72,7 +74,7 @@ function game_result(board) {
 }
 
 function place_move(board, player_no, col_index) {
-    if(!is_valid_col_range(col_index)) {
+    if (!is_valid_col_range(col_index)) {
         throw 'Invalid Move';
     }
 
@@ -87,4 +89,20 @@ function place_move(board, player_no, col_index) {
     board[row_index][col_index] = player_no;
 }
 
-module.exports = {is_game_over, place_move, fill_up, is_valid_in_board, game_result};
+function undo_move(board, move) {
+    if (!is_valid_col_range(col_index)) {
+        throw 'Invalid Column';
+    }
+
+    let row_index = 0;
+    while (row_index < 6 && board[row_index][col_index] === undefined) {
+        row_index += 1;
+    }
+
+    if (6 < row_index) {
+        throw 'Invalid Undo';
+    }
+    board[row_index][col_index] = undefined;
+}
+
+module.exports = { is_game_over, place_move, fill_up, is_valid_in_board, game_result, undo_move };
